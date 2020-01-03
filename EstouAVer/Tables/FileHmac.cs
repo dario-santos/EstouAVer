5ebb3632dc2fe8fd9623791e5d96186ec00e256a
Miguel Frias <miguelfrias16@gmail.com>
2020-01-03 17:28:09 +0000

    Correção no obj

--- EstouAVer/Tables/FileHmac.cs
+++ EstouAVer/Tables/FileHmac.cs
@@ -9,14 +9,12 @@ namespace EstouAVer.Tables
 
         public string path { get; }
         public string hmac { get; }
-
         public string UserName { get; }
 
         public FileHmac(string path, string hmac,  string UserNAme)
         {
             this.path = path;
             this.hmac = hmac;
-
             this.UserName = UserNAme;
         }
     }
