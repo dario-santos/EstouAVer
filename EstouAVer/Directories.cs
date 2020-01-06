75ca5971d8621a7524c7027bade1cd568e0775be
Miguel Frias <miguelfrias16@gmail.com>
2020-01-06 00:07:03 +0000

    Tirei o 2

--- EstouAVer/Directories.cs
+++ EstouAVer/Directories.cs
@@ -4,8 +4,8 @@ namespace EstouAVer
 {
     class Directories
     {
-        public static readonly string database = AppDomain.CurrentDomain.BaseDirectory    + "\\EstouAVer2.sqlite";
-        public static readonly string databaseAES = AppDomain.CurrentDomain.BaseDirectory + "\\EstouAVer2.aes";
+        public static readonly string database = AppDomain.CurrentDomain.BaseDirectory    + "\\EstouAVer.sqlite";
+        public static readonly string databaseAES = AppDomain.CurrentDomain.BaseDirectory + "\\EstouAVer.aes";
         public static readonly string servicePath = AppDomain.CurrentDomain.BaseDirectory + "\\EstouAVerlog.txt";
     }
 }
